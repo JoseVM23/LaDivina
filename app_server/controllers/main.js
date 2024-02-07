f738d4bd14015b4a7123e9850ead257421755b3e
Jose Luis Burga <jburga@estud.usfq.edu.ec>
2024-02-07 15:27:41 -0500

    Mi primer commit

---
+++ app_server/controllers/main.js
@@ -0,0 +1,13 @@
+// Estos son controllers
+
+const index = (req, res, next)=>{ 
+    res.render('index', { title: 'Mi primera app MERN',materia:'DW3' });//renderiza una vista index con el objeto jason titulo y materia
+  }
+  const usuarios = (req, res, next)=>{ 
+    res.render('usuarios', {});//renderiza una vista index con el objeto jason titulo y materia
+  }
+
+  module.exports = {
+    index, 
+    usuarios
+  }
